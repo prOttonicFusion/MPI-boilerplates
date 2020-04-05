cdf28a315ce624ebb1a76afbfc1186f2bf8ca33f
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-04-05 20:40:40 +0300

    added Fortran boilerplate

---
+++ Fortran/MPI_boilerplate.f90
@@ -0,0 +1,46 @@
+!
+! MPI Boilerplate for Fortran
+! @Author  : Otto Lindblom
+! @Link    : https://github.com/prOttonicFusion/MPI-boilerplates
+!
+
+program mpi_boilerplate
+
+use mpi
+implicit none
+
+  integer, parameter :: tag = 50
+  integer :: numproc, rank, namelength, ierr
+  character(len=22) :: procname = "MPI_MAX_PROCESSOR_NAME"
+
+  ! Initialize MPI
+  call mpi_init(ierr)
+  if (ierr /= 0) then 
+    print *, "MPI initialization failed"
+  end if
+
+  ! Query process information
+  call mpi_comm_size(MPI_COMM_WORLD, numproc, ierr);       ! numproc = number of processes/CPUs
+  call mpi_comm_rank(MPI_COMM_WORLD, rank, ierr);          ! rank = id of current process
+  call mpi_get_processor_name(procname, namelength, ierr); ! procname = name of current process
+
+  ! 
+  ! * MPI code goes here * *
+  ! 
+  ! Typical commands include: 
+  ! 
+  ! Send/receive data:
+  ! MPI_Send(void* data, int count, MPI_Datatype datatype, int destination, int tag, MPI_Comm communicator)
+  ! MPI_Recv(void* data, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm communicator, MPI_Status* status)
+  ! 
+  ! Reduce data from all processes to one (e.g. global sum when using op = MPI_SUM):
+  ! MPI_Reduce(void* send_data, void* recv_data, int count, MPI_Datatype datatype, MPI_Op op, int rootProcID, MPI_Comm communicator)
+  ! 
+  ! Send data to all processes:
+  ! MPI_Bcast(void* data, int count, MPI_Datatype datatype, int rootProcID, MPI_Comm communicator)
+  ! 
+
+  ! Finalize MPI & quit program
+  call mpi_Finalize(ierr)
+
+end program mpi_boilerplate
